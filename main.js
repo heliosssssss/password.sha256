b9c5646a8fb0efddf961c39c39dc859f21030e00
helios <heliosloljr@gmail.com>
2023-11-21 22:14:11 +0000

    final commit 11/21/23

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-import sha256 from "sha256";
-const prompt = require('prompt-sync')();
+const sha256 = require('sha256');
+const readline = require('readline');
 
-const password = prompt('enter your password')
-console.log(password)
+// TODO: save password to data, require login if there is saved_password, compare sha256 data to verify login
+// organize all code in func + add init func
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+r1.question('enter your password: ', (ans) => {
+    console.log("The password SHA256 is: " + sha256(ans));
+    r1.close();
+});
