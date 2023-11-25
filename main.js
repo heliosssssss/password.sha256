660b919370de69152b105c437b4e066362b078f0
helios <133915751+heliosssssss@users.noreply.github.com>
2023-11-25 16:46:34 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const sha256 = require('sha256');
 const readline = require('readline');
 
 // TODO: save password to data, require login if there is saved_password, compare sha256 data to verify login
-// organize all code in func + add init func
+// organize all code in func + add init function
 
 const r1 = readline.createInterface({
     input: process.stdin,
